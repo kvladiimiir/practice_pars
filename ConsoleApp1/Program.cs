f492bc99a2e84a7651f762464e92c2a621179049
Vladimir <kozyrev-vova@mail.ru>
2018-08-09 14:49:28 +0300

    Большое изменение, теперь проход по ссылкам идет сразу!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,12 +18,12 @@ namespace ConsoleApp1
 
             Logger.Info("Method GetCityEventList started ...");
             watch.Start();
-            var resultListEvent = GetCityEventList();
+            var resultListEvent = GetCityEventList(); //заполнение всех полей
             watch.Stop();
             Console.WriteLine($"Elapsed time {watch.Elapsed}");
 
             Logger.Info("Json write and serialized started ...");
-            string serializedCityEvents = JsonConvert.SerializeObject(resultListEvent);
+            string serializedCityEvents = JsonConvert.SerializeObject(resultListEvent); //вывод в файл json
             File.WriteAllText(@"result\CityEventData.json", serializedCityEvents);
             Logger.Info("Json write and serialized complete.");
 
@@ -32,17 +32,19 @@ namespace ConsoleApp1
 
         static List<CityEvent> GetCityEventList()
         {
-            List<CityEvent> rawEventList = new List<CityEvent>();
+            List<CityEvent> rawResultEventList = new List<CityEvent>();
 
             HtmlWeb web = new HtmlWeb();
             const int retryAttempsCount = 3;
+            int m = 1;
 
             var pageUrl = "https://kuda-kazan.ru/event/";
             string titlePhrase = "";
             do
             {
+                HtmlDocument docOfCityEvent = null;
                 HtmlDocument pageOfCityEvents = null;
-                for (int i = 0; i < retryAttempsCount; i++)
+                for (int i = 0; i < retryAttempsCount; i++) //получение страницы и проверка на её существование
                 {
                     pageOfCityEvents = web.Load(pageUrl);
 
@@ -63,47 +65,14 @@ namespace ConsoleApp1
                     newCityEvent.Url = n.Attributes["href"].Value;
                     var urlList = newCityEvent.Url.Split('/');
                     newCityEvent.ExternalId = urlList[urlList.Length - 2];
-                    rawEventList.Add(newCityEvent);
-                }
 
-                var nextNode = pageOfCityEvents.GetElementbyId("content").SelectSingleNode("//*[@class='next']");
-                pageUrl = nextNode.Attributes["href"].Value;
-                titlePhrase = nextNode.Attributes["title"].Value;
-            }
-            while ((titlePhrase != "Вы на последней странице") && (titlePhrase != ""));
-            Logger.Info("Method GetCityEventList complete!");
-
-            Stopwatch watch = new Stopwatch();
-            Logger.Info("Method FillCityEventsData started ...");
-            watch.Start();
-            var result = FillCityEventData(rawEventList);
-            watch.Stop();
-            Logger.Info("Method FillCityEventsData complete!");
-            Console.WriteLine($"Elapsed time {watch.Elapsed}");
-
-            return result;
-        }
-
-        static List<CityEvent> FillCityEventData(List<CityEvent> rawEventList)
-        {
-            List<CityEvent> resultEvents = new List<CityEvent>();
-
-            HtmlWeb web = new HtmlWeb();
-            int n = 1;
-            const int retryAttempsCount = 3;
-
-            foreach (CityEvent ev in rawEventList)
-            {
-                try
-                {
-                    HtmlDocument docOfCityEvent = null;
                     for (int i = 0; i < retryAttempsCount; i++)
                     {
-                        docOfCityEvent = web.Load(ev.Url);
+                        docOfCityEvent = web.Load(newCityEvent.Url);
 
                         if ((i + 1 == 3) && (docOfCityEvent == null))
                         {
-                            Logger.Error($"Can't get web event with url: {ev.Url}");
+                            Logger.Error($"Can't get web event with url: {newCityEvent.Url}");
                             break;
                         }
 
@@ -111,12 +80,12 @@ namespace ConsoleApp1
                             break;
                     }
 
-                    Console.WriteLine("Обработка события №" + n);
+                    Console.WriteLine("Обработка события №" + m);
 
                     var shortReviewNode = docOfCityEvent.GetElementbyId("short_review");
                     var startDateNode = shortReviewNode.SelectSingleNode("//*[@itemprop='startDate']");
 
-                    ev.StartDate = (startDateNode == null) ? throw new Exception("Failed to get 'StartDate'") : startDateNode.InnerText;
+                    newCityEvent.StartDate = (startDateNode == null) ? throw new Exception("Failed to get 'StartDate'") : startDateNode.InnerText;
 
                     var minPriceNode = shortReviewNode.SelectSingleNode("//*[@itemprop='price']");
 
@@ -128,45 +97,46 @@ namespace ConsoleApp1
                     {
                         if (minPriceNode.InnerText.Contains("от"))
                         {
-                            ev.MinPrice = int.Parse(minPriceNode.InnerText.Substring(minPriceNode.InnerText.IndexOf(' ') + 1));
+                            newCityEvent.MinPrice = int.Parse(minPriceNode.InnerText.Substring(minPriceNode.InnerText.IndexOf(' ') + 1));
                         }
                         else
                         {
-                            ev.MinPrice = (minPriceNode.InnerText == "0.00") ? 0 : int.Parse(minPriceNode.InnerText);
+                            newCityEvent.MinPrice = (minPriceNode.InnerText == "0.00") ? 0 : int.Parse(minPriceNode.InnerText);
                         }
                     }
 
                     var endDateNode = shortReviewNode.SelectSingleNode("//*[@itemprop='endDate']");
                     var endDateNodeText = endDateNode.InnerText;
 
-                    if ((endDateNode != null) && (ev.StartDate != endDateNodeText))
+                    if ((endDateNode != null) && (newCityEvent.StartDate != endDateNodeText))
                     {
-                        ev.EndDate = endDateNode.InnerText;
+                        newCityEvent.EndDate = endDateNode.InnerText;
                     }
 
                     var viewsCountNode = shortReviewNode.SelectSingleNode("//*[@class='wishes']/span[1]");
-                    ev.ViewsCount = (viewsCountNode == null) ? throw new Exception("Failed to get 'ViewsCount'") : int.Parse(viewsCountNode.InnerText);
+                    newCityEvent.ViewsCount = (viewsCountNode == null) ? throw new Exception("Failed to get 'ViewsCount'") : int.Parse(viewsCountNode.InnerText);
 
                     var contentNode = docOfCityEvent.GetElementbyId("content");
 
                     var nameNode = contentNode.SelectSingleNode("//h1");
-                    ev.Name = (nameNode == null) ? throw new Exception("Failed to get 'Name'") : WebUtility.HtmlDecode(nameNode.InnerText);
+                    newCityEvent.Name = (nameNode == null) ? throw new Exception("Failed to get 'Name'") : WebUtility.HtmlDecode(nameNode.InnerText);
 
                     var descriptionNode = contentNode.SelectSingleNode("//*[@itemprop='description']/p[1]");
-                    ev.Description = (descriptionNode == null) ? throw new Exception("Failed to get 'Description'") : WebUtility.HtmlDecode(descriptionNode.InnerText);
+                    newCityEvent.Description = (descriptionNode == null) ? throw new Exception("Failed to get 'Description'") : WebUtility.HtmlDecode(descriptionNode.InnerText);
 
-                    n++;
-                    resultEvents.Add(ev);
+                    m++;
+
+                    rawResultEventList.Add(newCityEvent);
                 }
 
-                catch (Exception ex)
-                {
-                    string guid = Guid.NewGuid().ToString();
-                    Logger.Error($"{ex.Message}");
-                    string filename = $"{guid}";
-                }
+                var nextNode = pageOfCityEvents.GetElementbyId("content").SelectSingleNode("//*[@class='next']"); //берем ссылку на следующую страницу
+                pageUrl = nextNode.Attributes["href"].Value;
+                titlePhrase = nextNode.Attributes["title"].Value;
             }
-            return resultEvents;
+            while ((titlePhrase != "Вы на последней странице") && (titlePhrase != ""));
+            Logger.Info("Method GetCityEventList complete!");
+
+            return rawResultEventList;
         }
     }
 }
